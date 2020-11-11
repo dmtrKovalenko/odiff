42b7ddfc213e7db411d2ce19ef8f1cab7a28a963
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2020-11-11 23:04:53 +0200

    Update version and description

--- scripts/post-release.js
+++ scripts/post-release.js
@@ -5,26 +5,27 @@ const releaseFolder = path.resolve(__dirname, "..", "_release");
 const packageJsonPath = path.join(releaseFolder, "package.json");
 const nodeBindingsPath = path.resolve(
   __dirname,
-  '..',
+  "..",
   "bin",
   "node-bindings",
   "odiff.js"
 );
 const nodeBindingsTsPath = path.resolve(
   __dirname,
-  '..',
+  "..",
   "bin",
   "node-bindings",
   "odiff.d.ts"
 );
 
-const { bin, ...package } = JSON.parse(
+const { bin, name, ...package } = JSON.parse(
   fs.readFileSync(packageJsonPath, {
     encoding: "utf-8",
   })
 );
 
 const packageJsonWithRightBinaryName = {
+  name: "odiff-bin",
   ...package,
   typings: "./odiff.d.ts",
   module: "./odiff.js",
