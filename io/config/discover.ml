ba5385c545be7de3f9d4440bef2b55ede0cdee72
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2021-01-20 11:28:47 +0200

    Run on CI

--- io/config/discover.ml
+++ io/config/discover.ml
@@ -7,6 +7,10 @@ let default : C.Pkg_config.package_conf =
   ; cflags = []
   }
 in
+let _static_or_not_flags = match Sys.os_type with
+  | "Unix" -> ["-static"]
+  | _ -> []
+in
 let conf =
   match C.Pkg_config.get c with
   | None -> default
@@ -14,10 +18,20 @@ let conf =
      match (C.Pkg_config.query pc ~package:"libpng") with
      | None -> default
      | Some deps -> deps
-in
 
+in
 Printf.printf "Resolved c flags %s" (String.concat "" conf.cflags);
 Printf.printf "\nResolved c libs %s" (String.concat "" conf.libs);
 
+ 
+Unix.environment ()
+|> Array.iter(fun env ->
+  if Str.string_match (Str.regexp "PNG") env 0 then
+    print_endline("\nlibpng env var:"^env)
+)
+|> ignore;
+
+
 C.Flags.write_sexp "c_flags.sexp"         conf.cflags;
 C.Flags.write_sexp "c_library_flags.sexp" conf.libs)
+
