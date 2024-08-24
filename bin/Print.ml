1a8ca30f203336ad40b113179a8df2403b6e68ca
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2024-08-24 12:41:51 +0200

    use spectrum for color output

--- bin/Print.ml
+++ bin/Print.ml
@@ -1,53 +1,32 @@
-(* open Odiff.Diff *)
+open Odiff.Diff
 
 let printDiffResult makeParsableOutput result =
-  (* (match (result, makeParsableOutput) with *)
-  (* | Layout, true -> "" *)
-  (* | Layout, false -> *)
-  (*     Pastel.createElement *)
-  (*       ~children: *)
-  (*         [ *)
-  (*           (Pastel.createElement ~color:Red ~bold:true ~children:[ "Failure!" ] *)
-  (*              () [@JSX]); *)
-  (*           " Images have different layout.\n"; *)
-  (*         ] *)
-  (*       () [@JSX] *)
-  (* | Pixel (_output, diffCount, _percentage, _lines), true when diffCount == 0 -> *)
-  (*     "" *)
-  (* | Pixel (_output, diffCount, _percentage, _lines), false when diffCount == 0 *)
-  (*   -> *)
-  (*     Pastel.createElement *)
-  (*       ~children: *)
-  (*         [ *)
-  (*           (Pastel.createElement ~color:Green ~bold:true *)
-  (*              ~children:[ "Success!" ] () [@JSX]); *)
-  (*           " Images are equal.\n"; *)
-  (*           (Pastel.createElement ~dim:true *)
-  (*              ~children:[ "No diff output created." ] *)
-  (*              () [@JSX]); *)
-  (*         ] *)
-  (*       () [@JSX] *)
-  (* | Pixel (_output, diffCount, diffPercentage, stack), true *)
-  (*   when not (Stack.is_empty stack) -> *)
-  (*     Int.to_string diffCount ^ ";" *)
-  (*     ^ Float.to_string diffPercentage *)
-  (*     ^ ";" *)
-  (*     ^ (stack *)
-  (*       |> Stack.fold (fun acc line -> (line |> Int.to_string) ^ "," ^ acc) "") *)
-  (* | Pixel (_output, diffCount, diffPercentage, _), true -> *)
-  (*     Int.to_string diffCount ^ ";" ^ Float.to_string diffPercentage *)
-  (* | Pixel (_output, diffCount, diffPercentage, _lines), false -> *)
-  (*     Pastel.createElement *)
-  (*       ~children: *)
-  (*         [ *)
-  (*           (Pastel.createElement ~color:Red ~bold:true ~children:[ "Failure!" ] *)
-  (*              () [@JSX]); *)
-  (*           " Images are different.\n"; *)
-  (*           "Different pixels: "; *)
-  (*           (Pastel.createElement ~color:Red ~bold:true *)
-  (*              ~children:[ Printf.sprintf "%i (%f%%)" diffCount diffPercentage ] *)
-  (*              () [@JSX]); *)
-  (*         ] *)
-  (*       () [@JSX]) *)
-  (* |> Console.log; *)
+  let reset_ppf = Spectrum.prepare_ppf Format.str_formatter in
+  (match (result, makeParsableOutput) with
+  | Layout, true -> ()
+  | Layout, false ->
+      Spectrum.Simple.printf "@{<red>%s!@} Images have different layout.\n"
+        "Failure!"
+  | Pixel (_output, diffCount, diffPercentage, stack), true
+    when not (Stack.is_empty stack) ->
+      Int.to_string diffCount ^ ";"
+      ^ Float.to_string diffPercentage
+      ^ ";"
+      ^ (stack
+        |> Stack.fold (fun acc line -> (line |> Int.to_string) ^ "," ^ acc) "")
+      |> print_endline
+  | Pixel (_output, diffCount, diffPercentage, _), true ->
+      Int.to_string diffCount ^ ";" ^ Float.to_string diffPercentage
+      |> print_endline
+  | Pixel (_output, diffCount, _percentage, _lines), false when diffCount == 0
+    ->
+      Spectrum.Simple.printf
+        "@{<green><bold>Success!@} Images are equal.\n\
+         @{<dim>No diff output created.@}"
+  | Pixel (_output, diffCount, diffPercentage, _lines), false ->
+      Spectrum.Simple.printf
+        "@{<red,bold>Failure!@} Images are different.\n\
+         Different pixels: @{<red,bold>%i (%f%%)@}" diffCount diffPercentage);
+
+  reset_ppf ();
   result
