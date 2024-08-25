e32c8cdfef2d9f5736252ba06f6eb401a81fc9a4
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2024-08-25 20:23:33 +0200

    release script

--- release.sh
+++ release.sh
@@ -3,6 +3,16 @@
 set -eou pipefail
 
 VERSION="$1"
+DRY_RUN=false
+
+for arg in "$@"
+do
+  if [ "$arg" = "--dry-run" ]; then
+    DRY_RUN=true
+    break
+  fi
+done
+
 if ! git diff --quiet; then
   echo "Error: There are unstaged changes in the repository."
   exit 1
@@ -11,16 +21,17 @@ fi
 sed -i '' "s/(version [^)]*)/(version $VERSION)/g" dune-project
 dune build 
 
-sed -i "s/\"version\": \"[^\"]*\"/\"version\": \"$VERSION\"/g" package.json
+sed -i '' "s/\"version\": \"[^\"]*\"/\"version\": \"$VERSION\"/g" package.json
 npm install
 
-sed -i "s/\"version\": \"[^\"]*\"/\"version\": \"$VERSION\"/g" npm_package/package.json
+sed -i '' "s/\"version\": \"[^\"]*\"/\"version\": \"$VERSION\"/g" npm_package/package.json
 
-git commit -m "chore(release): v$VERSION"
-
-git tag "v$VERSION"
-
-git push origin "v$VERSION"
-
-git push
-
+if [ "$DRY_RUN" == true ]; then
+  echo "Dry run, not committing or tagging"
+else
+  git add --all
+  git commit -m "chore(release): v$VERSION"
+  git tag "v$VERSION"
+  git push origin "v$VERSION"
+  git push
+fi
