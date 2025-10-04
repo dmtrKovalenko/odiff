c2ad13ba2b39c5db0f44cd8c1f8a718db3a6b9c6
Shreyas Mididoddi <me@shreyascodes.tech>
2025-10-04 21:41:18 +0530

    feat: add ci build step

--- build.zig
+++ build.zig
@@ -1,59 +1,16 @@
 const std = @import("std");
 const manifest = @import("build.zig.zon");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const dynamic = b.option(bool, "dynamic", "Link against libspng, libjpeg and libtiff dynamically") orelse false;
-
-    const lib_mod = b.createModule(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-        .link_libc = true,
-    });
-
-    linkDeps(b, target, optimize, dynamic, lib_mod);
-
-    var c_flags = std.array_list.Managed([]const u8).init(b.allocator);
-    defer c_flags.deinit();
-    c_flags.append("-std=c99") catch @panic("OOM");
-    c_flags.append("-Wno-nullability-completeness") catch @panic("OOM");
-    c_flags.append("-DHAVE_SPNG") catch @panic("OOM");
-    c_flags.append("-DSPNG_STATIC") catch @panic("OOM");
-    c_flags.append("-DSPNG_SSE=3") catch @panic("OOM");
-    c_flags.append("-DHAVE_JPEG") catch @panic("OOM");
-    c_flags.append("-DHAVE_TIFF") catch @panic("OOM");
-
-    lib_mod.addCSourceFiles(.{
-        .files = &.{
-            "c_bindings/odiff_io.c",
-        },
-        .flags = c_flags.items,
-    });
-
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    exe_mod.addImport("odiff_lib", lib_mod);
-
-    const options = b.addOptions();
-    options.addOption([]const u8, "version", manifest.version);
-    exe_mod.addImport("build_options", options.createModule());
-    lib_mod.addImport("build_options", options.createModule());
-
-    const exe = b.addExecutable(.{
-        .name = "odiff",
-        .root_module = exe_mod,
-    });
 
     const native_target = b.resolveTargetQuery(.{});
     const is_cross_compiling = target.result.cpu.arch != native_target.result.cpu.arch or
         target.result.os.tag != native_target.result.os.tag;
 
+    const lib_mod, const exe = buildOdiff(b, target, optimize, dynamic);
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
@@ -134,6 +91,73 @@ pub fn build(b: *std.Build) void {
     const test_all_step = b.step("test-all", "Run both unit and integration tests");
     test_all_step.dependOn(test_step);
     test_all_step.dependOn(integration_test_step);
+
+    const build_ci_step = b.step("ci", "Build the app for CI");
+    for (build_targets) |target_query| {
+        const t = b.resolveTargetQuery(target_query);
+        _, const odiff_exe = buildOdiff(b, t, optimize, dynamic);
+        odiff_exe.root_module.strip = true;
+        const odiff_output = b.addInstallArtifact(odiff_exe, .{
+            .dest_dir = .{
+                .override = .{
+                    .custom = try target_query.zigTriple(b.allocator),
+                },
+            },
+        });
+        build_ci_step.dependOn(&odiff_output.step);
+    }
+}
+
+fn buildOdiff(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+    dynamic: bool,
+) struct { *std.Build.Module, *std.Build.Step.Compile } {
+    const lib_mod = b.createModule(.{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    linkDeps(b, target, optimize, dynamic, lib_mod);
+
+    var c_flags = std.array_list.Managed([]const u8).init(b.allocator);
+    defer c_flags.deinit();
+    c_flags.append("-std=c99") catch @panic("OOM");
+    c_flags.append("-Wno-nullability-completeness") catch @panic("OOM");
+    c_flags.append("-DHAVE_SPNG") catch @panic("OOM");
+    c_flags.append("-DSPNG_STATIC") catch @panic("OOM");
+    c_flags.append("-DSPNG_SSE=3") catch @panic("OOM");
+    c_flags.append("-DHAVE_JPEG") catch @panic("OOM");
+    c_flags.append("-DHAVE_TIFF") catch @panic("OOM");
+
+    lib_mod.addCSourceFiles(.{
+        .files = &.{
+            "c_bindings/odiff_io.c",
+        },
+        .flags = c_flags.items,
+    });
+
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe_mod.addImport("odiff_lib", lib_mod);
+
+    const options = b.addOptions();
+    options.addOption([]const u8, "version", manifest.version);
+    exe_mod.addImport("build_options", options.createModule());
+    lib_mod.addImport("build_options", options.createModule());
+
+    const exe = b.addExecutable(.{
+        .name = "odiff",
+        .root_module = exe_mod,
+    });
+
+    return .{ lib_mod, exe };
 }
 
 pub fn linkDeps(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode, dynamic: bool, module: *std.Build.Module) void {
@@ -200,3 +224,12 @@ fn buildSpng(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.buil
     spng_lib.installHeader(spng_dep.path("spng/spng.h"), "spng.h");
     return spng_lib;
 }
+
+const build_targets: []const std.Target.Query = &.{
+    .{ .cpu_arch = .aarch64, .os_tag = .macos },
+    .{ .cpu_arch = .aarch64, .os_tag = .linux },
+    .{ .cpu_arch = .aarch64, .os_tag = .windows },
+    .{ .cpu_arch = .x86_64, .os_tag = .macos },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux },
+    .{ .cpu_arch = .x86_64, .os_tag = .windows },
+};
